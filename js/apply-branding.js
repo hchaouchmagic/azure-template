1927c7ad27398911397e8aceebd903529763a7cb
Hamza Chaouch <hamza.chaouch@celeste.fr>
2026-02-16 14:21:58 +0100

    debug reset link

--- js/apply-branding.js
+++ js/apply-branding.js
@@ -18,6 +18,7 @@ function applyBranding() {
             //now redirects to localhost but needs to be set dynamically via branding or domain based
             const redirectUri = "http://localhost:8080/auth/b2c";
             forgotPasswordLink.href = "https://clientsceleste.b2clogin.com/b33cf7f3-86b8-4845-be04-036de31663c8/oauth2/v2.0/authorize?p=B2C_1_Planet_icow_reset_password&client_id=c174dd34-b863-4a7c-a308-1eb08471e6a0&nonce=defaultNonce&redirect_uri="+redirectUri+"&prompt=login";
+            console.log("link password applied")
         }
     }
     //no brand default nuxit
