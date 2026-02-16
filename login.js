e965bfc3abb03dac41731f3f510e40577b8a1eaa
Hamza Chaouch <hamza.chaouch@celeste.fr>
2026-02-16 14:51:33 +0100

    update reset pwd link

--- login.js
+++ login.js
@@ -23,8 +23,8 @@ const forgotPasswordLink = document.getElementById("forgotPassword");
 if (forgotPasswordLink) {
     //redirect URL after password reset (mostly login page)
     //now redirects to localhost but needs to be set dynamically via branding or domain based
-    const redirectUri = "http%3A%2F%2Flocalhost:8080%2Fauth%2Fb2c";
-    forgotPasswordLink.href = "https://clientsceleste.b2clogin.com/b33cf7f3-86b8-4845-be04-036de31663c8/oauth2/v2.0/authorize?p=B2C_1_Planet_icow_reset_password&client_id=c174dd34-b863-4a7c-a308-1eb08471e6a0&nonce=defaultNonce&redirect_uri="+redirectUri+"&prompt=login";
+    const redirectUri = "http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fb2c";
+    forgotPasswordLink.href = "https://clientsceleste.b2clogin.com/b33cf7f3-86b8-4845-be04-036de31663c8/oauth2/v2.0/authorize?p=B2C_1_Planet_icow_reset_password&client_id=c174dd34-b863-4a7c-a308-1eb08471e6a0&nonce=defaultNonce&redirect_uri="+redirectUri+"&scope=openid&response_type=id_token&prompt=login";    
 }
 
 // Replace the input "password" to place it first in its parent (for tabulation)
