c1370d4ea8e1f314d784cac078156a68db6110dc
Hamza Chaouch <hamza.chaouch@celeste.fr>
2026-02-17 16:04:10 +0100

    dynamic reset password link

--- login.js
+++ login.js
@@ -24,8 +24,7 @@ if (forgotPasswordLink) {
     //redirect URL after password reset (mostly login page)
     const queryParams = new URLSearchParams(window.location.search);
     const redirectLink = queryParams.get("redirect_uri");
-    const redirectUri = "http%3A%2F%2Flocalhost%3A8080%2Fauth%2Fb2c";
-    console.log(redirectLink)
+    const redirectUri = redirectLink.replace("/callback", "");
     forgotPasswordLink.href = "https://clientsceleste.b2clogin.com/b33cf7f3-86b8-4845-be04-036de31663c8/oauth2/v2.0/authorize?p=B2C_1_Planet_icow_reset_password&client_id=c174dd34-b863-4a7c-a308-1eb08471e6a0&nonce=defaultNonce&redirect_uri="+redirectUri+"&scope=openid&response_type=id_token&prompt=login";    
 }
 
